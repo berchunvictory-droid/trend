509ffb53729c6b78619b8eccf3302a6b4b2be188
berchunvictory-droid <berchunvictory@gmail.com>
2026-03-26 16:08:28 +0100

    Update config.php

--- crkSettings/config.php
+++ crkSettings/config.php
@@ -1,6 +1,6 @@
 <?php return array (
-  'api_key' => '',
-  'offer_id' => 0,
-  'stream' => '',
+  'api_key' => 'bcfee0628c2634eab8c06377f99727c46f8d0b85f63a62f0e3906d354d852096',
+  'offer_id' => 74,
+  'stream' => 'z0vkExOK0D',
   'preland' => false,
 );
